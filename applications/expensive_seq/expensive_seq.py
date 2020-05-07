0d6e3e73f2fe5186a10362cc2d6ad56d3951c67b
David Tauraso <david318@me.com>
2020-05-07 13:48:43 -0700

    Did expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,5 +1,30 @@
+cache = {}
 def expensive_seq(x, y, z):
     # Implement me
+    global cache
+
+    if x <= 0:
+        return y + z
+    if x > 0:
+        # print(x, y, z)
+        # [print(i, cache[i]) for i in cache]
+        first_tuple = (x - 1, y + 1, z)
+        second_tuple = (x - 2, y + 2, z * 2)
+        third_tuple = (x - 3, y + 3, z * 3)
+
+        # allows the result of one of the tuples to have an answer dependent
+        # on a previous tuple being calculated
+
+        if  first_tuple not in cache:
+            cache[first_tuple] = expensive_seq(x - 1, y + 1, z)
+        if second_tuple not in cache:
+            cache[second_tuple] = expensive_seq(x - 2, y + 2, z * 2)
+        if third_tuple not in cache:
+            cache[third_tuple] = expensive_seq(x - 3, y + 3, z * 3)
+
+        return  cache[first_tuple] + \
+                cache[second_tuple] + \
+                cache[third_tuple]
 
 if __name__ == "__main__":
     for i in range(10):
